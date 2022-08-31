a1da5869fade9c6bb68feaf88d65295bf2154edf
Andrei <andruxametelkin@gmail.com>
2022-08-31 17:50:34 +0400

    Complete

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] ThreeCharacters(string[] words)
+{
+    string[] result = new string[words.Length];
+
+    int numelement = 0;
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length <= 3)
+        {
+            result[numelement] = words[i];
+            numelement++;
+        }
+    }
+
+    return result;
+}
+
+void ShowString(string[] words)
+{
+    for (int i = 0; i < words.Length; i++)
+        Console.Write($"{words[i]}  ");
+    Console.WriteLine();
+}
+
+string[] nani = { "ohayo", "hi", "Naruto", "pain", "si" };
+
+ShowString(ThreeCharacters(nani));
